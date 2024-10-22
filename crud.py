a545bb942c3e78652ca1c1c6299c375484e53d91
jagernau <stepanenkov25@gmail.com>
2024-10-22 15:33:14 +0300

    универсальное отображение объектов из систем мониторинга

--- crud.py
+++ crud.py
@@ -1,6 +1,45 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, outerjoin
 from mysql_models import *
+from sqlalchemy import case
 
 
 def get_all_objects(db: Session):
-    return db.query(CaObject).all()
+    result = db.query(
+                CaObject.id,
+                MonitoringSystem.mon_sys_name,
+                CaObject.object_name,
+                ObjectStatus.status,
+                CaObject.owner_user,
+                CaObject.imei,
+                Device.device_serial,
+                DevicesBrand.name.label('brand_name'),
+                SimCard.sim_tel_number,
+                case(
+                    [
+                        (SimCard.status == 0, "Списана"),
+                        (SimCard.status == 1, "Активна"),
+                        (SimCard.status == 2, "Приостановлена"),
+                        (SimCard.status == 3, "Первичная блокировка"),
+                        (SimCard.status == 4, "Статус не известен"),
+                    ]
+                ).label("sim_status"),
+                Contragent.ca_uid_contragent,
+                Contragent.ca_name_contragent,
+                Contragent.ca_inn,
+                Contragent.ca_kpp,
+            ).outerjoin(
+                MonitoringSystem, CaObject.sys_mon_id == MonitoringSystem.mon_sys_id
+            ).outerjoin(
+                ObjectStatus, CaObject.object_status == ObjectStatus.status_id
+            ).outerjoin(
+                Device, CaObject.imei == Device.device_imei
+            ).outerjoin(
+                DevicesBrand, Device.devices_brand_id == DevicesBrand.id
+            ).outerjoin(
+                SimCard, CaObject.imei == SimCard.terminal_imei
+            ).outerjoin(
+                Contragent, CaObject.contragent_id == Contragent.ca_id
+            ).all()
+    db.close()
+    return result
+
